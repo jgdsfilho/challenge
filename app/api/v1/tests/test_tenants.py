601bb423aaf4ba976c0e63353374b181f95e8053
Genilson Filho <genilson974@gmail.com>
2025-01-14 23:19:04 -0300

    Create route to get tenants

--- app/api/v1/tests/test_tenants.py
+++ app/api/v1/tests/test_tenants.py
@@ -64,3 +64,70 @@ async def test_fail_create_tenant_when_already_exist_tenant(
     assert response.json() == {
         "detail": "A unique constraint violation occurred, please check your input.",
     }
+
+
+@pytest.mark.asyncio
+async def test_get_tenant_with_id(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+):
+    payload = {"name": "tenant1", "email": "tenant1@my_domain.com"}
+    response = await async_client.post(
+        "/tenants",
+        json=payload,
+    )
+    assert response.status_code == 200
+    tenant_id = response.json()["id"]
+
+    response = await async_client.get(f"/tenants/{tenant_id}")
+    assert response.status_code == 200
+    for key, value in payload.items():
+        assert response.json()[key] == value
+
+
+@pytest.mark.asyncio
+async def test_get_tenant_with_email(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+):
+    payload = {"name": "tenant1", "email": "tenant1@my_domain.com"}
+    response = await async_client.post(
+        "/tenants",
+        json=payload,
+    )
+    assert response.status_code == 200
+
+    response = await async_client.get(f"/tenants/?email={payload['email']}")
+    assert response.status_code == 200
+    for key, value in payload.items():
+        assert response.json()[key] == value
+
+
+@pytest.mark.asyncio
+async def test_fail_get_tenant_with_email_not_exist(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+):
+    response = await async_client.get("/tenants/?email=tenant_not_exist@my_domain.com")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Tenant not found"}
+
+
+@pytest.mark.asyncio
+async def test_update_tenant(
+    async_client: AsyncClient,
+    async_session: AsyncSession,
+):
+    payload = {"name": "tenant1", "email": "tenant1@my_domain.com"}
+    response = await async_client.post(
+        "/tenants",
+        json=payload,
+    )
+    assert response.status_code == 200
+    tenant_id = response.json()["id"]
+
+    payload["name"] = "tenant1_updated"
+    response = await async_client.patch(f"/tenants/{tenant_id}", json=payload)
+    assert response.status_code == 200
+    for key, value in payload.items():
+        assert response.json()[key] == value
